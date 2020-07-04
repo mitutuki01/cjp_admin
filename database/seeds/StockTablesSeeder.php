18f2330f3c41dc641ea338ac4ed0ca2ba4e47aee
M.Kagura <mitutuki01@gmail.com>
2020-07-05 08:56:49 +0900

    [DB] add seeder

--- database/seeds/StockTablesSeeder.php
+++ database/seeds/StockTablesSeeder.php
@@ -11,6 +11,56 @@ class StockTablesSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Fakerを使う
+        $faker = Faker\Factory::create('ja_JP');
+
+        DB::table('mst_product')->insert([
+            'product_id' => 1,
+            'name' => 'モデラート',
+            'image' => '',
+        ]);
+        DB::table('mst_product')->insert([
+            'product_id' => 2,
+            'name' => '酒器だるま',
+            'image' => '',
+        ]);
+
+        DB::table('mst_genre')->insert([
+            'genre_id' => 1,
+            'name' => 'EC',
+        ]);
+        DB::table('mst_genre')->insert([
+            'genre_id' => 2,
+            'name' => 'カタログ',
+        ]);
+
+        DB::table('stock')->insert([
+            'product_id' => 1,
+            'genre_id' => 1,
+            'stock' => 10,
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+        ]);
+        DB::table('stock')->insert([
+            'product_id' => 1,
+            'genre_id' => 2,
+            'stock' => 5,
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+        ]);
+        DB::table('stock')->insert([
+            'product_id' => 2,
+            'genre_id' => 1,
+            'stock' => 2,
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+        ]);
+        DB::table('stock')->insert([
+            'product_id' => 1,
+            'genre_id' => 2,
+            'stock' => 7,
+            'created_at' => $faker->dateTime(),
+            'updated_at' => $faker->dateTime(),
+        ]);
     }
 }
