f9799225f266b852d217e709be8e9bd2a7916502
M.Kagura <mitutuki01@gmail.com>
2020-07-05 10:20:38 +0900

    [DB] add spec

--- database/migrations/2020_07_04_154300_create_mst_products_table.php
+++ database/migrations/2020_07_04_154300_create_mst_products_table.php
@@ -17,7 +17,9 @@ class CreateMstProductsTable extends Migration
             $table->increments('product_id');
             $table->string('name');
             $table->string('image');
+            $table->integer('jan_code');
             $table->index('name');
+            $table->index('jan_code');
         });
     }
 
